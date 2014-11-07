5a8d7dc2074b7f8847ce4079e07078e544e03617
Maxim Avanov <maxim.avanov@gmail.com>
2014-11-07 22:39:02 +0300

    Fix formatting

--- harry_merge.ml
+++ harry_merge.ml
@@ -7,24 +7,27 @@ let handle_command output_src filenames () =
 
 (* define a custom input argument type *)
 let text_file_t = 
-    Command.Spec.Arg_type.create(
-        fun filename -> match Sys.is_file filename with
-        | `Yes -> filename
-        | `No | `Unknown ->
-            eprintf "'%s' is not a regular file.\n%!" filename;
-            exit 1
-    )
+    Command.Spec.Arg_type.create
+        (fun filename -> match Sys.is_file filename with
+                        | `Yes -> filename
+                        | `No | `Unknown ->
+                            eprintf "'%s' is not a regular file.\n%!" filename;
+                            exit 1
+        )
 
 
 let cmd_spec = 
     let open Command.Spec in
         (* verify input parameters *)
-        step (fun m output_src filenames ->
-              match filenames with
-              | [] | _ :: [] -> eprintf "You must specify at least two files.\n%!";
-                                exit 1
-              | _ -> m output_src filenames
-        )
+        step
+            (fun m output_src filenames ->
+                 (* The first m argument to the step callback is the next callback function in the chain. *)
+                 match filenames with
+                 | [] | _ :: [] -> eprintf "You must specify at least two files.\n%!";
+                                   exit 1
+                 | _ -> m output_src filenames
+            )
+
         +> flag "-o" (optional string) ~doc: "string Output to file rather than stdout"
         +> anon (sequence ("filename" %: text_file_t))
 
